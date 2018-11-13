92593a5755244981d2614a896a02d3ecb19c08e3
Flávio Silvério <flaviosilverio@Flavios-iMac-2.local>
2018-11-13 10:16:14 +0000

    Fixed collisions

--- GameSpriteKit/GameScene.swift
+++ GameSpriteKit/GameScene.swift
@@ -11,9 +11,9 @@ import GameplayKit
 
 struct ColliderType {
 
-    static let Player: UInt32 = 1
-    static let Enemy: UInt32 = 2
-    static let Screen: UInt32 = 3
+    static let Player: UInt32 = 0x1 << 1
+    static let Enemy: UInt32 = 0x1 << 2
+    static let Screen: UInt32 = 0x1 << 3
 
 }
 
@@ -30,9 +30,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     override func didMove(to view: SKView) {
 
-
-        self.anchorPoint = CGPoint(x: 0.5,y: 0.5)
         self.physicsWorld.contactDelegate = self
+
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody?.friction = 0
+        //        self.physicsBody?.affectedByGravity = false
+        //        self.physicsBody?.isDynamic = true
+        self.physicsBody?.categoryBitMask = ColliderType.Screen
+        self.physicsBody?.collisionBitMask = ColliderType.Player
 
         player = SKSpriteNode(imageNamed: "mario")
         player.name = "Player"
@@ -43,13 +48,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.physicsBody?.affectedByGravity = false
         player.physicsBody?.isDynamic = true
         player.physicsBody?.categoryBitMask = ColliderType.Player
-        player.physicsBody?.collisionBitMask = ColliderType.Enemy | ColliderType.Screen
+        player.physicsBody?.collisionBitMask = ColliderType.Screen
         player.physicsBody?.contactTestBitMask = ColliderType.Enemy | ColliderType.Screen
 
         self.addChild(player)
-
-//        self.physicsBody?.friction = 0
-//        self.physicsBody?.categoryBitMask = ColliderType.Screen
 
         pointsLabel = self.childNode(withName: "label") as! SKLabelNode
 
@@ -86,8 +88,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         enemy.position = CGPoint(x: CGFloat(number2), y: 100)
         enemy.physicsBody = SKPhysicsBody(rectangleOf: player.size)
         enemy.physicsBody?.affectedByGravity = true
-        enemy.physicsBody?.isDynamic = true
         enemy.physicsBody?.categoryBitMask = ColliderType.Enemy
+        enemy.physicsBody?.collisionBitMask = ColliderType.Player
         enemy.physicsBody?.friction = 10.0
 
         self.addChild(enemy)
@@ -122,44 +124,46 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             secondBody.node?.removeFromParent()
             score += score + 1
 
+            print("item")
+
         } else {
             print("walls")
         }
 
     }
-    
-    
+
+
     func touchDown(atPoint pos : CGPoint) {
 
     }
-    
+
     func touchMoved(toPoint pos : CGPoint) {
 
     }
-    
+
     func touchUp(atPoint pos : CGPoint) {
 
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
         direction = direction * -1
 
     }
-    
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
-    
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
-    
+
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
-    
-    
+
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         player.position.x = player.position.x + CGFloat(direction * 2)
